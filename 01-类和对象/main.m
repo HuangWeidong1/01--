7bca1151593a064f5051b34c219d0c0be60a0ca4
赵凌云 <zhaolingyun@zhao.local>
2015-08-14 13:07:38 +0800

    Initial Commit

---
+++ 01-类和对象/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  01-类和对象
+//
+//  Created by 赵凌云 on 15/8/14.
+//  Copyright (c) 2015年 赵凌云. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
